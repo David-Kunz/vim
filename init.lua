57d4be346f4ea6a0c1e64d82558f65093eca57c0
D065023 <david.kunz@sap.com>
2021-11-29 09:17:24 +0100

    cleaner

--- init.lua
+++ init.lua
@@ -640,10 +640,10 @@ vim.cmd('iabbrev darkgreen #006400')
 -- map('n', '<s-y>', ':lua require"tree-mover".current()<CR>')
 
 
-local buf_of_tab = {}
+_G.term_buf_of_tab = _G.term_buf_of_tab or {}
 _G.toggle_terminal = function()
   local cur_tab = vim.api.nvim_get_current_tabpage()
-  local term_buf = buf_of_tab[cur_tab]
+  local term_buf = term_buf_of_tab[cur_tab]
   if term_buf ~= nil then
    local cur_buf = vim.api.nvim_get_current_buf()
    if cur_buf == term_buf then
@@ -655,7 +655,7 @@ _G.toggle_terminal = function()
   else
     vim.cmd('vs | terminal')
     local cur_buf = vim.api.nvim_get_current_buf()
-    table.insert(buf_of_tab, cur_tab, cur_buf)
+    table.insert(term_buf_of_tab, cur_tab, cur_buf)
     vim.cmd(':startinsert')
   end
 end
