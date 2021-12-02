584380a63c4a6b96c7b5da2eab13467d63b17190
D065023 <david.kunz@sap.com>
2021-12-02 11:53:39 +0100

    jester

--- init.lua
+++ init.lua
@@ -438,9 +438,9 @@ map('n', '<leader>gl', ':Git log<cr>')
 map('n', '<leader>gp', ':Git push<cr>')
 
 -- David-Kunz/jester
-map('n', '<leader>tt', ':lua require"jester".run()<cr>')
-map('n', '<leader>t_', ':lua require"jester".run_last()<cr>')
-map('n', '<leader>tf', ':lua require"jester".run_file()<cr>')
+map('n', '<leader>tt', ':lua require"jester".run({ path_to_jest = "/usr/local/bin/jest" })<cr>')
+map('n', '<leader>t_', ':lua require"jester".run_last({ path_to_jest = "/usr/local/bin/jest" })<cr>')
+map('n', '<leader>tf', ':lua require"jester".run_file({ path_to_jest = "/usr/local/bin/jest" })<cr>')
 map('n', '<leader>dd', ':lua require"jester".debug({ path_to_jest = "/usr/local/bin/jest" })<cr>')
 map('n', '<leader>d_', ':lua require"jester".debug_last({ path_to_jest = "/usr/local/bin/jest" })<cr>')
 map('n', '<leader>df', ':lua require"jester".debug_file({ path_to_jest = "/usr/local/bin/jest" })<cr>')
