e79e6f7ee4a2aa026062465c57a5fa9135a3402a
D065023 <david.kunz@sap.com>
2022-02-08 08:06:11 +0100

    better

--- init.lua
+++ init.lua
@@ -104,29 +104,26 @@ map('n', '<leader>v', ':e $MYVIMRC<CR>')
 require('gitsigns').setup({
   current_line_blame = true,
   on_attach = function(bufnr)
-    local gs = package.loaded.gitsigns
-    _G.gs = package.loaded.gitsigns
-
-    -- Navigation
+       -- Navigation
     map('n', ']c', "&diff ? ']c' : '<cmd>Gitsigns next_hunk<CR>'", {expr=true})
     map('n', '[c', "&diff ? '[c' : '<cmd>Gitsigns prev_hunk<CR>'", {expr=true})
 
     -- Actions
     map('n', '<leader>hs', ':Gitsigns stage_hunk<CR>')
-    map('n', '<leader>hr', ':Gitsigns reset_hunk<CR>')
     map('v', '<leader>hs', ':Gitsigns stage_hunk<CR>')
+    map('n', '<leader>hr', ':Gitsigns reset_hunk<CR>')
     map('v', '<leader>hr', ':Gitsigns reset_hunk<CR>')
-    map('n', '<leader>hS', ':lua gs.stage_buffer()<CR>')
-    map('n', '<leader>hu', ':lua gs.undo_stage_hunk()<CR>')
-    map('n', '<leader>hR', ':lua gs.reset_buffer()<CR>')
-    map('n', '<leader>hp', ':lua gs.preview_hunk()<CR>')
-    map('n', '<leader>hb', ':lua gs.blame_line{full=true}<CR>')
-    map('n', '<leader>tb', ':lua gs.toggle_current_line_blame()<CR>')
-    map('n', '<leader>hd', ':lua gs.diffthis()<CR>')
-    map('n', '<leader>hD', ":lua gs.diffthis('~')<CR>")
-    map('n', '<leader>td', ':lua gs.toggle_deleted()<CR>')
+    map('n', '<leader>hS', '<cmd>Gitsigns stage_buffer<CR>')
+    map('n', '<leader>hu', '<cmd>Gitsigns undo_stage_hunk<CR>')
+    map('n', '<leader>hR', '<cmd>Gitsigns reset_buffer<CR>')
+    map('n', '<leader>hp', '<cmd>Gitsigns preview_hunk<CR>')
+    map('n', '<leader>hb', '<cmd>lua require"gitsigns".blame_line{full=true}<CR>')
+    map('n', '<leader>tb', '<cmd>Gitsigns toggle_current_line_blame<CR>')
+    map('n', '<leader>hd', '<cmd>Gitsigns diffthis<CR>')
+    map('n', '<leader>hD', '<cmd>lua require"gitsigns".diffthis("~")<CR>')
+    map('n', '<leader>td', '<cmd>Gitsigns toggle_deleted<CR>')
 
-    -- -- Text object
+    -- Text object
     map('o', 'ih', ':<C-U>Gitsigns select_hunk<CR>')
     map('x', 'ih', ':<C-U>Gitsigns select_hunk<CR>')
   end
