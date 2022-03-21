c3feaa6056d3a36c1819cd58dbb3a5d4906b54ae
D065023 <david.kunz@sap.com>
2022-03-21 09:37:43 +0100

    fix

--- init.lua
+++ init.lua
@@ -723,11 +723,12 @@ _G.term_buf_of_tab = _G.term_buf_of_tab or {}
 _G.term_buf_max_nmb = _G.term_buf_max_nmb or 0
 
 function spawn_terminal()
+  local cur_tab = vim.api.nvim_get_current_tabpage()
   vim.cmd('vs | terminal')
   local cur_buf = vim.api.nvim_get_current_buf()
   _G.term_buf_max_nmb = _G.term_buf_max_nmb + 1
   vim.api.nvim_buf_set_name(cur_buf, "Terminal " .. _G.term_buf_max_nmb)
-  table.insert(term_buf_of_tab, cur_tab, cur_buf)
+  table.insert(_G.term_buf_of_tab, cur_tab, cur_buf)
   vim.cmd(':startinsert')
 end
 
