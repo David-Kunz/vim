0e49253d2a3c56cfb882a91ec618a4072e4d5c0c
D065023 <david.kunz@sap.com>
2022-04-15 17:18:59 +0200

    better git signs

--- init.lua
+++ init.lua
@@ -121,6 +121,13 @@ _G.diffThisBranch = function()
 end
 
 require('gitsigns').setup({
+  signs = {
+    add          = {hl = 'GitSignsAdd'   , text = '▌', numhl='GitSignsAddNr'   , linehl='GitSignsAddLn'},
+    change       = {hl = 'GitSignsChange', text = '▌', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
+    delete       = {hl = 'GitSignsDelete', text = '▁', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
+    topdelete    = {hl = 'GitSignsDelete', text = '▔', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
+    changedelete = {hl = 'GitSignsChange', text = '~', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
+  },
   current_line_blame = true,
   on_attach = function(bufnr)
        -- Navigation
