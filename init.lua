ca738d6b024dc68922a4f389d3647b7c78c21796
Dr. David A. Kunz <david.kunz@sap.com>
2023-11-22 16:49:16 +0100

    .

--- init.lua
+++ init.lua
@@ -451,7 +451,7 @@ vim.g.floaterm_width = 0.95
 vim.g.floaterm_height = 0.95
 vim.keymap.set('n', '<leader>g', ':FloatermNew lazygit<CR>')
 
-cmd('set foldmethod=indent')
+-- cmd('set foldmethod=indent')
 -- cmd('set foldmethod=expr')
 -- cmd('set foldexpr=nvim_treesitter#foldexpr()')
 
